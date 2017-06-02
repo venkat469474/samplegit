c936e007c3893d3899945cb37d6af8b104b11ca5
venkat <venkat@DESKTOP-SS5HA7D>
2017-06-03 00:07:16 +0530

    first commit

---
+++ samplegit/src/com/git/first/Main.java
@@ -0,0 +1,17 @@
+package com.git.first;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+salesdata data = new salesdata();
+displaygreeting();
+data.display();
+	}
+private static void displaygreeting()
+{
+	
+System.out.println("ok ok");
+
+}
+}
---
+++ samplegit/src/com/git/first/salesdata.java
@@ -0,0 +1,14 @@
+package com.git.first;
+
+public class salesdata {
+	int data[] = {0,4,42};
+	
+	
+
+	public void display()
+	{
+		System.out.print("implement dada display");
+		
+	}
+	
+}
